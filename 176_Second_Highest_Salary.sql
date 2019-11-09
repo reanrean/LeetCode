425dc1cf09d24b47913dd9e141fec41eaec7935b
reanrean <rinnerean@yahoo.co.jp>
2019-11-09 03:01:19 -0500

    176

--- 176_Second_Highest_Salary.sql
+++ 176_Second_Highest_Salary.sql
@@ -1,13 +1,15 @@
-# Write your MySQL query statement below
 select max(Salary) as SecondHighestSalary from 
     (select * from Employee a where a.Salary <> (select max(Salary) from Employee)) b;
 
-# solution on leetcode:
+/*solution on leetcode:*/
 SELECT
     (SELECT DISTINCT
             Salary
         FROM
             Employee
         ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1) AS SecondHighestSalary
-;
+        LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+
+select max(Salary) as SecondHighestSalary
+from Employee
+where Salary < (select max(Salary) from Employee);
